fcf1751a2f2a38f5e44d9142036f5557752cf4da
598455803 <598455803@qq.com>
2017-04-12 11:48:13 +0800

    test

---
+++ src/main/java/com/example/HelloController.java
@@ -0,0 +1,16 @@
+package com.example;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ * Created by fj on 2017/4/12.
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    public String sayHello(){
+        System.out.println("hello world");
+        return  "hello spring boot";
+    }
+}
